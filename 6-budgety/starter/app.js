c66ea92912a585d1d2e3375edbbab83257c110bd
Andras Szesztai <andras@newzoo.com>
2019-05-25 17:41:24 +0200

    Added event listeners

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,18 +1,16 @@
-const budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  }
 
-  return {
-    publicTest: function(b){
-      return add(b)
-    }
-  }
+//// BUDGET CONTROLLER
+const budgetController = (function() {
+
+
+
 })();
 
 
 
+
+
+//// UI CONTROLLER
 const uiController = (function() {
 
 
@@ -20,17 +18,40 @@ const uiController = (function() {
 })()
 
 
+
+
+
+//// GLOBAL APP CONTROLLER
 const controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5)
+  const ctrlAddItem = function() {
 
-  return {
-    anotherPublic: function(){
-      console.log(z)
+    // 1. Get input data
+
+    // 2. Add the item to the budget CONTROLLER
+
+    // 3. Add the new item to UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
     }
-  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function(event) {
+
+    if(event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem()
+    }
+
+  })
 
 })(budgetController, uiController);
+
+
+
 
 
 
