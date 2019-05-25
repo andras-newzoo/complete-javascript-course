9a9ab13a737148978d7535952b754602e24f9228
Andras Szesztai <andras@newzoo.com>
2019-05-25 22:25:11 +0200

    Added clearfields function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -63,7 +63,9 @@ const uiController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   }
 
   return {
@@ -76,6 +78,42 @@ const uiController = (function() {
               value: document.querySelector(domStrings.inputValue).value
       }
 
+    },
+
+    addListItem: function(obj, type) {
+      let html, newHtml, element
+
+      // Create HTML string with placeholder text
+      if(type === 'inc') {
+        element = domStrings.incomeContainer
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      } else if (type === 'exp') {
+        element = domStrings.expensesContainer
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+
+      // Replace the placeholder text with some actual data
+      newHtml = html.replace('%id%', obj.id)
+      newHtml = newHtml.replace('%description%', obj.description)
+      newHtml = newHtml.replace('%value%', obj.value)
+
+      // Insert HTML into the domStrings
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
+
+    },
+
+    clearFields: function() {
+      let fields, fieldsArr
+
+      fields = document.querySelectorAll(domStrings.inputDescription + ', ' + domStrings.inputValue)
+
+      fieldsArr = Array.prototype.slice.call(fields)
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = ""
+      })
+
+      fieldsArr[0].focus()
     },
 
     getDOMStrings: function() {
@@ -119,6 +157,10 @@ const controller = (function(budgetCtrl, uiCtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value)
 
     // 3. Add the new item to UI
+    uiCtrl.addListItem(newItem, input.type)
+
+    // Clear the fields
+    uiCtrl.clearFields()
 
     // 4. Calculate the budget
 
